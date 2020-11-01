05fb926394e91742c86204f6097040061dfc5e4a
suneel93 <suneel@msewa.com>
2020-11-01 12:46:49 +0530

    Updates

---
+++ precache-manifest.54b5de183072f8ad522d991c000a4f5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5de4ef71fe0632fc26e",
+    "url": "/portfolio/static/css/main.0c3a5907.chunk.css"
+  },
+  {
+    "revision": "e5de4ef71fe0632fc26e",
+    "url": "/portfolio/static/js/main.d41bb337.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "12d61f938d2c61722105",
+    "url": "/portfolio/static/css/2.064d81ec.chunk.css"
+  },
+  {
+    "revision": "12d61f938d2c61722105",
+    "url": "/portfolio/static/js/2.3c7d430e.chunk.js"
+  },
+  {
+    "revision": "74ffe45b1351edb1920f4b237ba73581",
+    "url": "/portfolio/static/media/suneel.74ffe45b.png"
+  },
+  {
+    "revision": "10db746f9941dd66690003696d52efc1",
+    "url": "/portfolio/index.html"
+  }
+];
